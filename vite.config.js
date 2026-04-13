752e2e13f21526a065d313d7ea3829a62ea9615b
xxxxxneonxxxxx <xneoshax@gmail.com>
2026-04-13 20:22:26 +0700

    g

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,13 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base:  '/hz/', 
+
   plugins: [
     vue(),
     vueDevTools(),
   ],
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
